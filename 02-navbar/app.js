2a4da3eb9351891d26fc80c7ad6f731f98f78f6e
thisisyjin <hnesb970@gmail.com>
2022-03-10 21:01:46 +0900

    init ver

---
+++ 02-navbar/app.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector(".toogle");
+const menu = document.querySelector(".menus-grp");
+const icons = document.querySelector(".icons-grp");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  icons.classList.toggle("active");
+  toggleBtn.classList.toggle("active");
+});
